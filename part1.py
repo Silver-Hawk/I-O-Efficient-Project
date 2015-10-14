ac984fa56d124d4ed96851c33e39c31f49d56d48
Jesper Lund <jbl@sundhedsdoktor.dk>
2015-10-14 16:32:17 +0200

    Rearranged part1.py to be able to flush on each result.

--- part1.py
+++ part1.py
@@ -6,12 +6,17 @@ heaplist = [[0 for x in range(5)] for x in range(26)]
 i = 1000
 lasti = 0
 index = 0
+print("QUICK")
 while lasti < 500000000:
 	for j in range(5):
-		print("QUICK : I " + str(i) + " j " + str(j))
 		proc = Popen("java -Xmx4g Tester quicksortints " +str(i), shell=True, stdout=PIPE, stderr=PIPE)
 		proc.wait()
 		quicklist[index][j] = float(proc.stdout.readline())
+	
+	quicklist[index].sort()
+	mean = (quicklist[index][1]+quicklist[index][2]+quicklist[index][3])/3
+	print(str(i)+","+str(mean))
+	
 	lasti = i
 	i = int(i * 1.7)
 	index += 1
@@ -19,31 +24,17 @@ while lasti < 500000000:
 i = 1000
 lasti = 0
 index = 0
+print("HEAP")
 while lasti < 500000000:
 	for j in range(5):
-		print("HEAP : I " + str(i) + " j " + str(j))
 		proc = Popen("java -Xmx4g Tester heapsortints " +str(i), shell=True, stdout=PIPE, stderr=PIPE)
 		proc.wait()
-		quicklist[index][j] = float(proc.stdout.readline())
+		heaplist[index][j] = float(proc.stdout.readline())
+	
+	heaplist[index].sort()
+	mean = (heaplist[index][1]+heaplist[index][2]+heaplist[index][3])/3
+	print(str(i)+","+str(mean))
+	
 	lasti = i
 	i = int(i * 1.7)
 	index += 1
-
-
-i = 1000
-for x in range(len(quicklist)):
-	quicklist[x].sort()
-	#for y in range(len(quicklist[x])):
-	#	print(quicklist[x][y])
-	mean = (quicklist[x][1]+quicklist[x][2]+quicklist[x][3])/3
-	print(str(i)+","+str(mean))
-	i = int(i * 1.7)
-
-i = 1000
-for x in range(len(heaplist)):
-	heaplist[x].sort()
-	#for y in range(len(quicklist[x])):
-	#	print(quicklist[x][y])
-	mean = (heaplist[x][1]+heaplist[x][2]+heaplist[x][3])/3
-	print(str(i)+","+str(mean))
-	i = int(i * 1.7)
